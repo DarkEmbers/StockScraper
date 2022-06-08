0187e802a4515833a96b034f2b18d95dde380a84
DarkEmbers <tsharihar@gmail.com>
2022-06-09 00:26:44 +0400

    Added scheduling and custom interval in config file

--- main.py
+++ main.py
@@ -1,84 +1,105 @@
-import time
 import yfinance as yf
-import threading
-from openpyxl import Workbook, worksheet, load_workbook
+from openpyxl import load_workbook
+import logging
 import json
+import threading
+import time
+import schedule
 import os
 import sys
-import schedule
 
-workbook = None
-sheet = None
-sheetPath = ""
-threads = []
-
-
+'''
+	Get stats from yahoo finance 
+	for each ticker symbol in the spreadsheet
+'''
 def get_stats(ticker, row_count):
-	# get the stock data
-	info = yf.Tickers(ticker).tickers[ticker].info
-	update_stock_price(info['currentPrice'], row_count)
-	# print(f"{ticker}::{info['currentPrice']}")
+    info = yf.Tickers(ticker).tickers[ticker].info
+    update_stock_price(info['currentPrice'], row_count)
 
 
+'''
+	Access and modify the cell values of column B
+	with new stock prices
+'''
 def update_stock_price(price, row_count):
-	# write the stock price to the cell values of column B
-	sheet.cell(row=row_count, column=2).value = price
-	workbook.save(sheetPath)
+    sheet.cell(row=row_count, column=2).value = price
+    workbook.save(sheetPath)  # Save the changes to the spreadsheet
 
 
 def main():
-	global workbook, sheet, sheetPath
+    global workbook, sheet, sheetPath, config_path
+    threads = []
 
-	try:
-		# load config.json and get excel sheet path
-		with open(config_path) as f:
-			config = json.load(f)
-			sheetPath = config["path"]
+    # Load the config file and spreadsheet
+    try:
+        with open(config_path) as f:
+            config = json.load(f)
+            sheetPath = config["path"]
 
-	except FileNotFoundError:
-		print("Config file not found")
-		return
+    except FileNotFoundError:
+        logging.error("Config file not found")
+        return
 
-	try:
-		workbook = load_workbook(sheetPath)
-		sheet = workbook.active
+    try:
+        workbook = load_workbook(sheetPath)
+        sheet = workbook.active
 
-	except FileNotFoundError:
-		print("Excel file not found")
-		return
+    except FileNotFoundError:
+        logging.error("Excel file not found")
+        return
 
-	# get the ticker symbols from the first column of the spreadsheet
-	row_count = 2
-	while True:
-		# Read the cell values of column A
-		ticker = sheet.cell(row=row_count, column=1).value
-		if ticker is None:
-			break
+    '''
+		Go through each ticker symbol in column A
+		and get the stats for that symbol
+	'''
+    row_count = 2
+    while True:
+        # Read the cell values of column A
+        ticker = sheet.cell(row=row_count, column=1).value
+        if ticker is None:
+            break
 
-		# create a new thread for each ticker symbol
-		thread = threading.Thread(target=get_stats, args=[ticker, row_count])
-		thread.start()
-		threads.append(thread)
-		row_count += 1
+        # Create a new thread for each ticker symbol
+        thread = threading.Thread(target=get_stats, args=[ticker, row_count])
+        thread.start()
+        threads.append(thread)
+        row_count += 1
+
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
+
+    date_time = time.strftime("%d/%m/%Y %H:%M:%S")
+    logging.info(f"Updated {date_time}")  # Log the time of the update
 
 
 if __name__ == '__main__':
-	# determine if the application is a .exe
-	if getattr(sys, 'frozen', False):
-		application_path = os.path.dirname(sys.executable)
-	# or a script file
-	elif __file__:
-		application_path = os.path.dirname(__file__)
-		config_name = 'StonksConfig.json'
-		config_path = os.path.join(application_path, config_name)
+    logging.basicConfig(level=logging.INFO)
 
-	main()
-	for thread in threads:
-		thread.join()
+    # Determine if the application is a .exe
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+    # Or a script file
+    elif __file__:
+        application_path = os.path.dirname(__file__)
 
-	print(f"Updated {time.clock_gettime(time.CLOCK_REALTIME)}")
+    # Get the path to the config file
+    config_name = 'StonksConfig.json'
+    config_path = os.path.join(application_path, config_name)
 
-	# schedule.every(15).minutes.do(main)
-	# while True:
-	# 	schedule.run_pending()
-	# 	time.sleep(1)
+    try:
+        # Load config.json and get interval value
+        with open(config_path) as f:
+            config = json.load(f)
+            interval = config["interval"]
+
+    except FileNotFoundError:
+        logging.critical("Config file not found")
+        sys.exit(1)
+
+    # Run every "interval" minutes
+    main()
+    schedule.every(interval).minutes.do(main)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
